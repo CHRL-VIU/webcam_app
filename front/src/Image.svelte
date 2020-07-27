3e323abdee65f0bdf1b65be068b0770221d37bcb
Alex Cebulski <Alex@Sandys-Macbook.local>
2020-07-27 16:49:53 -0700

    update to grab images remotely and remove backslash from html

--- front/src/Image.svelte
+++ front/src/Image.svelte
@@ -2,7 +2,7 @@
 import {images} from './data-images.js'
 import Image from "svelte-image"
 export let image = {}
-const imgPath = 'http://localhost/'
+const imgPath = 'http://www.viu-hydromet-wx.ca/webcam_images/'
 </script>
 
 <div class = "image"> 
--- front/src/Images.svelte
+++ front/src/Images.svelte
@@ -4,7 +4,7 @@ import {onMount} from 'svelte'
 import Image from './Image.svelte'
 
 onMount( async() => {
-    const url = 'http://localhost/get-images.php'
+    const url = 'http://www.viu-hydromet-wx.ca/webcam_images/get-images.php'
     let res = await fetch(url)
     res = await res.json()
     //console.log(res)
